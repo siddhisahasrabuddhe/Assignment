c304df1e3594fa06e47ce7682d062e2baae98208
siddhisahasrabuddhe <67067789+siddhisahasrabuddhe@users.noreply.github.com>
2021-02-02 04:41:51 -0800

    Add files via upload

---
+++ Que10.java
@@ -0,0 +1,14 @@
+package assignment;
+
+public class Que10 {
+
+	public static void main(String[] args) {
+		
+		
+		for(int num=1;num<=5;num++)
+	     {
+	     System.out.print((num*num*num)+" ");     
+	    }
+	}
+
+}
